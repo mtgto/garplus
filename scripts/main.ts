15b79b36e3b0438268c9a0d56a8c257dca3d4ba2
mtgto <hogerappa@gmail.com>
2016-11-06 00:33:07 +0900

    ちょっと修正

--- scripts/main.ts
+++ scripts/main.ts
@@ -8,12 +8,12 @@ if (textarea) {
             // u4E00-u9FCF: CJK Unified Ideographs
             // uFF00-uFFEF: Halfwidth and Fullwidth Forms
             var urlPattern: RegExp = /(https?:\/\/[\w!#$%&'\(\)*+,-./:;=?@\[\]_|~\u3040-\u30ff\u4e00-\u9fa0\uff00-\uffef]+)([\s"<>\\^`{}]?)/g;
-            var newValue = textarea.value.replace(urlPattern, function(match, url, last) {
+            var newValue = textarea.value.replace(urlPattern, function(match: string, url: string, last: string) {
                 return new URL(url).toString() + last;
             });
             if (textarea.value != newValue) {
                 textarea.value = newValue;
             }
-        }, 10);
+        }, 10); // pasteイベント発生時はペーストされた文字列が取得されないのでちょっと待ってから取得する
     });
 }
