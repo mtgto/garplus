413bc33c03135c1efc6ac664f57717c668bcd3e0
mtgto <hogerappa@gmail.com>
2018-07-19 09:08:47 +0900

    Update to latest.
    - Fix to work chrome current api.
    - Adopt editorconfig, prettier.
    - Use webpack for all deployment tasks.

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,37 +1,51 @@
-/// <reference path="../node_modules/@types/chrome/index.d.ts" />
-"use strict";
-import consts = require("./consts");
-var textarea: HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById('textarea_id');
-var privateSchedules: Text[] = [];
+import * as consts from "./consts";
+
+type TextWithMaskedText = Text & { _maskedText: string };
+const textarea: HTMLTextAreaElement = document.getElementById(
+    "textarea_id",
+) as HTMLTextAreaElement;
+const privateSchedules: TextWithMaskedText[] = [];
 if (textarea) {
-    textarea.addEventListener('paste', function(e) {
-        setTimeout(function() {
+    textarea.addEventListener("paste", e => {
+        setTimeout(() => {
             // u3040-u309F: Hiragana
             // u30A0-u30FF: Katakana
             // u4E00-u9FCF: CJK Unified Ideographs
             // uFF00-uFFEF: Halfwidth and Fullwidth Forms
-            var urlPattern: RegExp = /(https?:\/\/[\w!#$%&'\(\)*+,-./:;=?@\[\]_|~\u3040-\u30ff\u4e00-\u9fa0\uff00-\uffef]+)([\s"<>\\^`{}]?)/g;
-            var newValue = textarea.value.replace(urlPattern, function(match: string, url: string, last: string) {
-                return new URL(url).toString() + last;
-            });
+            const urlPattern: RegExp = /(https?:\/\/[\w!#$%&'\(\)*+,-./:;=?@\[\]_|~\u3040-\u30ff\u4e00-\u9fa0\uff00-\uffef]+)([\s"<>\\^`{}]?)/g;
+            const newValue = textarea.value.replace(
+                urlPattern,
+                (match: string, url: string, last: string) => {
+                    return new URL(url).toString() + last;
+                },
+            );
             if (textarea.value !== newValue) {
                 textarea.value = newValue;
             }
         }, 10); // pasteイベント発生時はペーストされた文字列が取得されないのでちょっと待ってから取得する
     });
 }
-Array.prototype.forEach.call(document.querySelectorAll("div.normalEventElement>div>a>img"), function(img: HTMLImageElement) {
-    if (img.src.indexOf("privateMark") !== -1) {
-        var privateSchedule: Text = <Text>img.previousSibling;
-        privateSchedule["_maskedText"] = privateSchedule.textContent;
-        privateSchedule.textContent = chrome.i18n.getMessage("stringHiddenTitle");
-        privateSchedules.push(privateSchedule);
-    }
-});
-chrome.runtime.onMessage.addListener(function(message, sender: chrome.runtime.MessageSender, sendResponse) {
-    if (message["menu"] === consts.CONTEXT_MENU_SHOW_MASKED_SCHEDULE) {
-        for (let privateSchedule of privateSchedules) {
-            privateSchedule.textContent = privateSchedule["_maskedText"];
+Array.prototype.forEach.call(
+    document.querySelectorAll("div.normalEventElement>div>a>img"),
+    (img: HTMLImageElement) => {
+        if (img.src.indexOf("privateMark") !== -1) {
+            const privateSchedule: TextWithMaskedText = img.previousSibling as TextWithMaskedText;
+            if (privateSchedule.textContent) {
+                privateSchedule._maskedText = privateSchedule.textContent;
+                privateSchedule.textContent = chrome.i18n.getMessage(
+                    "stringHiddenTitle",
+                );
+            }
+            privateSchedules.push(privateSchedule);
         }
-    }
-});
+    },
+);
+chrome.runtime.onMessage.addListener(
+    (message: any, sender: chrome.runtime.MessageSender, sendResponse) => {
+        if (message.menu === consts.ContextMenuShowMaskedSchedule) {
+            for (const privateSchedule of privateSchedules) {
+                privateSchedule.textContent = privateSchedule._maskedText;
+            }
+        }
+    },
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,51 @@
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-  entry: {
-    main: './scripts/main.js',
-    background: './scripts/background.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + '/dist/scripts'
-  }
-}
+    devtool: 'cheap-module-source-map',
+    entry: {
+        main: "./scripts/main.ts",
+        background: "./scripts/background.ts",
+    },
+    output: {
+        filename: "[name].js",
+        path: `${__dirname}/dist/scripts`,
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                include: `${__dirname}/scripts`,
+                use: ["awesome-typescript-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            GAROON_BASE_URL: JSON.stringify(process.env.GAROON_BASE_URL),
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: "manifest-template.json",
+                to: `${__dirname}/dist/manifest.json`,
+                transform (content, path) {
+                    if (process.env.GAROON_BASE_URL) {
+                        return content.toString().replace(
+                            /GAROON_BASE_URL/,
+                            process.env.GAROON_BASE_URL,
+                        ).replace(/PACKAGE_JSON_VERSION/, require("./package.json").version);
+                    } else {
+                        throw new Error("process.env.GAROON_BASE_URL is not defined.");
+                    }
+                },
+            },
+        ]),
+    ],
+    optimization: {
+        minimizer: [new UglifyJsPlugin({})],
+    },
+};
